5ae3d475e5f6944938a53b180e8e0618c572dc0e
nicholas.silva <bnsilva5@misena.edu.co>
2024-07-28 16:16:33 -0500

    Actividad 2 parte final y adelanto parte del proyecto final

--- src/components/SearchApi.jsx
+++ src/components/SearchApi.jsx
@@ -14,28 +14,37 @@ const TrackSearch = () => {
 
   return (
     <>
-      <div>
-        <h1>NyxPotify Search</h1>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={trackName}
-              onChange={(e) => setTrackName(e.target.value)}
-              placeholder="Enter track name"
-            />
-            <button type="submit">Search</button>
-          </form>
-          <ul>
-            {tracks.map(track => (
-              <li key={track.id}>
+     <div className="track-search-container">
+      <h1 className="custom-title">NyxPotify Search</h1>
+      <form onSubmit={handleSubmit} className="search-form">
+        <input
+          type="text"
+          value={trackName}
+          onChange={(e) => setTrackName(e.target.value)}
+          placeholder="Enter track name"
+          className="search-input"
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+      <div className="track-arc-container">
+        <ul className="track-list">
+          {tracks.map(track => (
+            <li key={track.id} className="track-item">
+              <div className="track-content">
                 <img src={track.album.images[0]?.url} alt={track.name} width="100" />
-                <div>{track.name} by {track.artists[0].name}</div>
-              </li>
-            ))}
-          </ul>
-        </div>
+                <div className="track-details">
+                  <div className="track-name">{track.name}</div>
+                  <div className="track-artist">{track.artists[0].name}</div>
+                  <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="spotify-button">
+                    Play on Spotify
+                  </a>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
+    </div>
     </>
   );
 };
